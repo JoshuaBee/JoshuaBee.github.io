d31538d724fef5e498a5854511c1b98185d288ad
Joshua Bee <joshuaregular@gmail.com>
2026-02-24 15:37:43 +0000

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        background_color: "#a300c8",
+        description: "Personal website for Joshua Bee",
+        display: "standalone",
+        icons: [
+          {
+            src: "images/cards/home/joshua-bee-electric-192.webp",
+            type: "image/webp",
+            sizes: "192x192",
+            purpose: "any maskable"
+          },
+          {
+            src: "images/cards/home/joshua-bee-electric-512.webp",
+            type: "image/webp",
+            sizes: "512x512",
+            purpose: "any maskable"
+          },
+        ],
+        name: "Joshua Bee",
+        screenshots: [
+          {
+            src: "images/screenshots/screenshot1.webp",
+            sizes: "5120x2156",
+            type: "image/webp",
+            platform: "wide",
+            label: "Homescreen of Joshua Bee"
+          }
+        ],
+        short_name: "Joshua Bee",
+        theme_color: "#a300c8"
+      }
+    })
+  ]
+});
